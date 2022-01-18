da88a75316b3bc1dd8c5a7df6a31f78a95b4b975
punarvi <punarvi.punnu@gmail.com>
2022-01-19 01:38:10 +0530

    First table added

--- src/app/reactiveformdemo/reactiveformdemo.component.ts
+++ src/app/reactiveformdemo/reactiveformdemo.component.ts
@@ -9,12 +9,41 @@ import { FormBuilder } from '@angular/forms';
 export class ReactiveformdemoComponent implements OnInit {
 
   closerDropdownData:any = ["Kim Shields","Rhonda Williams","Shela Bungartz-Vail","Mary Ellen Tellez"];
+
+  lockType:any = ["Standard Lock (2 day Fixed)","15 Day","Relock (Worst Case Pricing)","Lock Extension (3 day)","N/A"];
+
+  mipPercentage:any = ["0.5","2%","2.5%"];
+
+  productType:any = ["Fixed","Annual","Annual Low Cost","HECM 999 Annual","4500 Annual 400","4500 Annual 400","Equity Edge Fixed","Equity Edge Low Cost"];
+
+  marginRate:any =["1.24","1.25","1.365","1.375","1.50","1.625","1.75","1.875","2.0"];
+
+  pricingEcception:any = ["yes","no"];
+
+  exceptionType:any = ["Origination Fee Waiver","Lock Extension","Other (please indicate in Notes)"];
+
+  approvedBy:any = ["Richard Thorpe","Patrick Lambrecht","David Peskin","Oscar Moure","Eric Ellsworth"];
+
+  productChange:any = ["Yes*","No"]
+  
   constructor(public fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
   reverseWorksheetForm = this.fb.group({
-    closerSelect:[]
+    closerSelect:[],
+    dateType:[],
+    loanOfficer:[],
+    borrower:[],
+    loanNo:[],
+    lockTypeDropDown:[],
+    mipPercentageDropDown:[],
+    productTypeDropDown:[],
+    marginRateDropDown:[],
+    pricingEcceptionDropDown:[],
+    exceptionTypeDropDown:[],
+    approvedByDropDown:[],
+    productChangeDropDown:[]
   })
 }
